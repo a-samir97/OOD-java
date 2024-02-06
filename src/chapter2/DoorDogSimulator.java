4697ad958b31a2e359af354db4f01b2f38d9f91e
Ahmed Samir <a.samir9710@gmail.com>
2024-02-06 23:37:05 +0200

    finished the dog door application with some improvements in alternative paths

--- src/chapter2/DoorDogSimulator.java
+++ src/chapter2/DoorDogSimulator.java
@@ -5,16 +5,21 @@ public class DoorDogSimulator {
         DogDoor door = new DogDoor();
         Remote remote = new Remote(door);
 
-        System.out.println("Fido barks to go outside ...");
+        System.out.println();
         remote.pressButton();
 
         System.out.println("\nFido has gone outside ...");
-        remote.pressButton();
 
         System.out.println("\nFido's all done ....");
+
+        try{
+            Thread.currentThread().wait(1000);
+        } catch (Exception e) { }
+        System.out.println("... but he's stuck outside ..");
+        System.out.println("\nFido starts barking ...");
+        System.out.println("\nSo customer grabs the remote control to open the door");
         remote.pressButton();
 
         System.out.println("\nFido back inside ...");
-        remote.pressButton();
     }
 }
